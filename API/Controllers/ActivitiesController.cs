91eab4f544a21d1290fe5cc0ce5b3f4f49d06861
Utsav Kuinkel <utshaav583@gmail.com>
2021-05-28 20:49:25 +0545

    Bug fixed

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -32,7 +32,7 @@ namespace API.Controllers
             return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
         }
 
-        [HttpPost("{id}")]
+        [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
